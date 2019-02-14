c2782ee54999b72f1c515c5c1bda714f0d41fe7c
Armand McQueen <armandmcqueen@gmail.com>
2019-02-13 17:34:00 -0800

    Allow venv to be passed in as command line argument

--- no_batch_train_1node_8gpu.sh
+++ no_batch_train_1node_8gpu.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/env bash
 
+VENV=${1:-tensorflow_p36}
+
 HOROVOD_TIMELINE=/home/ubuntu/logs/htimeline.json \
 HOROVOD_CYCLE_TIME=0.5 \
 HOROVOD_FUSION_THRESHOLD=67108864 \
-/home/ubuntu/anaconda3/envs/tensorflow_p36/bin/mpirun -np 8 \
+/home/ubuntu/anaconda3/envs/${VENV}/bin/mpirun -np 8 \
 -H localhost:8 \
 --mca plm_rsh_no_tree_spawn 1 -bind-to none -map-by slot -mca pml ob1 -mca btl ^openib \
 -mca btl_tcp_if_exclude lo,docker0 \
 -mca btl_vader_single_copy_mechanism none \
 -x NCCL_SOCKET_IFNAME=^docker0,lo \
--x NCCL_MIN_NRINGS=8 -x NCCL_DEBUG=INFO \
+-x NCCL_MIN_NRINGS=3 -x NCCL_DEBUG=INFO \
 -x LD_LIBRARY_PATH -x PATH \
 -x HOROVOD_CYCLE_TIME -x HOROVOD_FUSION_THRESHOLD \
 --output-filename /home/ubuntu/logs/mpirun_logs \
-/home/ubuntu/anaconda3/envs/tensorflow_p36/bin/python3 /home/ubuntu/tensorpack-mask-rcnn/MaskRCNN-no-batch/train.py \
+/home/ubuntu/anaconda3/envs/${VENV}/bin/python3 /home/ubuntu/tensorpack-mask-rcnn/MaskRCNN-no-batch/train.py \
 --logdir /home/ubuntu/logs/train_log \
 --perf \
 --throughput_log_freq 20000 \
