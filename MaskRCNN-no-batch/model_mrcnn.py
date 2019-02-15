f6204c20c24cf700f651bd15414049bf345f186a
Victor Suthichai <victor.suthichai@gmail.com>
2019-02-15 07:46:36 +0000

    Turn off fp16 mrcnn frcnn

--- MaskRCNN-no-batch/model_mrcnn.py
+++ MaskRCNN-no-batch/model_mrcnn.py
@@ -66,8 +66,8 @@ def maskrcnn_upXconv_head(feature, num_category, num_convs, norm=None):
     """
     assert norm in [None, 'GN'], norm
     l = feature
-    l = tf.cast(l, tf.float16)
-    with mixed_precision_scope():
+    #l = tf.cast(l, tf.float16)
+    with mixed_precision_scope(mixed=False):
       with argscope([Conv2D, Conv2DTranspose], data_format='channels_first',
                   kernel_initializer=tf.variance_scaling_initializer(
                       scale=2.0, mode='fan_out',
@@ -79,7 +79,7 @@ def maskrcnn_upXconv_head(feature, num_category, num_convs, norm=None):
                 l = GroupNorm('gn{}'.format(k), l)
         l = Conv2DTranspose('deconv', l, cfg.MRCNN.HEAD_DIM, 2, strides=2, activation=tf.nn.relu)
         l = Conv2D('conv', l, num_category, 1)
-    l = tf.cast(l, tf.float32)
+    #l = tf.cast(l, tf.float32)
     return l
 
 
