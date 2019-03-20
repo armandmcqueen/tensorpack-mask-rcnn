e173058a6a6e754192509ba81e4e0ecbbfa2a7ef
Armand McQueen <armandmcqueen@gmail.com>
2019-03-20 16:57:24 -0700

    Updated tf event download script for tensorboard

--- docker/pull_tboard_comparisons.sh
+++ docker/pull_tboard_comparisons.sh
@@ -6,7 +6,9 @@
 
 # Use in VM, not inside docker container
 
+rm -r ~/old_logs
 mkdir -p ~/old_logs
+
 
 aws s3 cp --recursive s3://aws-tensorflow-benchmarking/maskrcnn/results/nobatch_notconverging_20190315_t1 ~/old_logs/nobatch_notconverging_20190315_t1
 aws s3 cp --recursive s3://aws-tensorflow-benchmarking/maskrcnn/results/cuda10_baseline_converge_2019021_3030540 ~/old_logs/cuda10_baseline_converge_2019021_3030540
@@ -14,11 +16,15 @@ aws s3 cp --recursive s3://aws-tensorflow-benchmarking/maskrcnn/results/cuda10_b
 
 # Convergence isolation codebase
 aws s3 cp --recursive s3://aws-tensorflow-benchmarking/maskrcnn/results/convergence_codebase_iso_baseline_20190319 ~/old_logs/convergence_codebase_iso_baseline_20190319
-aws s3 cp --recursive s3://aws-tensorflow-benchmarking/maskrcnn/results/convergence_codebase_iso_genproposals_20190319 ~/old_logs/convergence_codebase_iso_genproposals_20190319
+aws s3 cp --recursive s3://aws-tensorflow-benchmarking/maskrcnn/results/convergence_codebase_iso_baseline_20190320 ~/old_logs/convergence_codebase_iso_baseline_20190320
 aws s3 cp --recursive s3://aws-tensorflow-benchmarking/maskrcnn/results/convergence_codebase_iso_roialign_box_20190319 ~/old_logs/convergence_codebase_iso_roialign_box_20190319
+aws s3 cp --recursive s3://aws-tensorflow-benchmarking/maskrcnn/results/convergence_codebase_iso_roialign_mask_20190320 ~/old_logs/convergence_codebase_iso_roialign_box_20190320
+aws s3 cp --recursive s3://aws-tensorflow-benchmarking/maskrcnn/results/convergence_codebase_iso_sampletargets_20190320 ~/old_logs/convergence_codebase_iso_sampletargets_20190320
+aws s3 cp --recursive s3://aws-tensorflow-benchmarking/maskrcnn/results/convergence_codebase_iso_rpnloss_20190320 ~/old_logs/convergence_codebase_iso_rpnloss_20190320
 
 
 
 
 # sudo rm ~/logs/htimeline.json
 # aws s3 cp --recursive logs/ s3://aws-tensorflow-benchmarking/maskrcnn/results/convergence_codebase_iso_XXXXXXX/
+
