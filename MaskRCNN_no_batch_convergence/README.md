ae39dafe39dc0426bcffeec42ba59381f29265e5
Armand McQueen <armandmcqueen@gmail.com>
2019-03-22 15:23:56 -0700

    Updated convergence readme and tensorboard event list

--- MaskRCNN_no_batch_convergence/README.md
+++ MaskRCNN_no_batch_convergence/README.md
@@ -6,7 +6,7 @@ Before committing to master, we must test that the new pieces leads to convergen
 
 ## Combined Pieces
 
-### All that have been tested
+### Bratin data 20190321
 
 BATCH_GENERATE_PROPOSALS = True
 
@@ -20,44 +20,50 @@ BATCH_CROP_AND_RESIZE_MASK = True
 BATCH_MASK_LOSS = True
 
 Converges TTA on bbox but not segm
+
+Throughput was measured on p3dn, accuracy on p3.16xl
+
 66 img/s
 
 
 
 
+### Layer 2, Block 1
 
-### Set 1
+BATCH_DATA_PIPELINE
+Include BATCH_GENERATE_PROPOSALS? Depends on when this is completed.
+
+
+### Layer 2, Block 2
 
 BATCH_GENERATE_PROPOSALS
+BATCH_RPN_HEAD
 BATCH_RPN_LOSS
 
-Need to run this once BATCH_RPN_LOSS has been independently fixed. Maybe throw in rpn_head too
+Need to run this once BATCH_RPN_LOSS has been independently fixed. Maybe throw in BATCH_RPN_HEAD too
 
-### Set 2
+### Layer 2, Block 3
 
 BATCH_SAMPLE_TARGETS
 BATCH_ROI_ALIGN_BOX
 
-Ran on Node 6 (need to push tfevents)
 Converges
 
 
-### Set 3
+### Layer 2, Block 4
 
 BATCH_FAST_RCNN_OUTPUTS
 BATCH_FAST_RCNN_LOSSES
+Converges, maybe slightly (0.4-0.6%) low on small segmentation
 
-Ran on Node 7 (need to send tfevents)
-Converges
 
-### Set 4
+### Layer 2, Block 5
 
 BATCH_ROI_ALIGN_MASK
 BATCH_CROP_AND_RESIZE_MASK
 BATCH_MASK_LOSS
 
-Running on Node 8
-Converged on bbox but not segm
+Did not converge on SEGME. Converged on BBOX
 
 
 
@@ -67,8 +73,8 @@ Converged on bbox but not segm
 
 ### All disabled
 
-Converges
-Running on Node 2 to check with 9 flags, all disabled.
+Converges with 9 flags. 
+Need to repeat as more flags are added
 
 
 ### GenerateProposals 
@@ -88,13 +94,12 @@ Converges. Maybe 0.3% off on small objects. Probably deserves a repeat later
 ### Sample targets
 
 Fixed issue where randomness was disabled. 
-Ran on Node 1 (need to copy tfevents)
 Converges
 
 ### RPN loss
 
 No longer certain. 
-Running on Node 5
+Running on Node 5. Seems to crash a lot.
 
 
 
--- docker/pull_tboard_comparisons.sh
+++ docker/pull_tboard_comparisons.sh
@@ -14,23 +14,45 @@ aws s3 cp --recursive s3://aws-tensorflow-benchmarking/maskrcnn/results/nobatch_
 aws s3 cp --recursive s3://aws-tensorflow-benchmarking/maskrcnn/results/cuda10_baseline_converge_2019021_3030540 ~/old_logs/cuda10_baseline_converge_2019021_3030540
 
 
-# Convergence isolation codebase
+# Convergence isolation codebase -
 aws s3 cp --recursive s3://aws-tensorflow-benchmarking/maskrcnn/results/convergence_codebase_iso_baseline_20190319 ~/old_logs/convergence_codebase_iso_baseline_20190319
 aws s3 cp --recursive s3://aws-tensorflow-benchmarking/maskrcnn/results/convergence_codebase_iso_baseline_20190320 ~/old_logs/convergence_codebase_iso_baseline_20190320
+aws s3 cp --recursive s3://aws-tensorflow-benchmarking/maskrcnn/results/convergence_codebase_iso_baseline_20190322 ~/old_logs/convergence_codebase_iso_baseline_20190322
 aws s3 cp --recursive s3://aws-tensorflow-benchmarking/maskrcnn/results/convergence_codebase_iso_roialign_box_20190319 ~/old_logs/convergence_codebase_iso_roialign_box_20190319
 aws s3 cp --recursive s3://aws-tensorflow-benchmarking/maskrcnn/results/convergence_codebase_iso_roialign_mask_20190320 ~/old_logs/convergence_codebase_iso_roialign_box_20190320
-aws s3 cp --recursive s3://aws-tensorflow-benchmarking/maskrcnn/results/convergence_codebase_iso_sampletargets_20190320 ~/old_logs/convergence_codebase_iso_sampletargets_20190320
 aws s3 cp --recursive s3://aws-tensorflow-benchmarking/maskrcnn/results/convergence_codebase_iso_rpnloss_20190320 ~/old_logs/convergence_codebase_iso_rpnloss_20190320
 aws s3 cp --recursive s3://aws-tensorflow-benchmarking/maskrcnn/results/convergence_codebase_iso_fastrcnn_losses_20190321 ~/old_logs/convergence_codebase_iso_fastrcnn_losses_20190321
 aws s3 cp --recursive s3://aws-tensorflow-benchmarking/maskrcnn/results/convergence_codebase_iso_fastrcnn_outputs_20190321 ~/old_logs/convergence_codebase_iso_fastrcnn_outputs_20190321
 aws s3 cp --recursive s3://aws-tensorflow-benchmarking/maskrcnn/results/convergence_codebase_iso_maskloss_20190321 ~/old_logs/convergence_codebase_iso_maskloss_20190321
 aws s3 cp --recursive s3://aws-tensorflow-benchmarking/maskrcnn/results/convergence_codebase_iso_crop_and_resize_mask_20190321 ~/old_logs/convergence_codebase_iso_crop_and_resize_mask_20190321
+aws s3 cp --recursive s3://aws-tensorflow-benchmarking/maskrcnn/results/convergence_codebase_iso_sampletargets_20190322 ~/old_logs/convergence_codebase_iso_sampletargets_20190322
+aws s3 cp --recursive s3://aws-tensorflow-benchmarking/maskrcnn/results/convergence_codebase_iso_sampletargets_and_roialignbox_20190322 ~/old_logs/convergence_codebase_iso_sampletargets_and_roialignbox_20190322
+aws s3 cp --recursive s3://aws-tensorflow-benchmarking/maskrcnn/results/convergence_codebase_iso_fastrcnnoutputs_and_fastrcnnlosses_20190322 ~/old_logs/convergence_codebase_iso_fastrcnnoutputs_and_fastrcnnlosses_20190322
+
+
+
+# Convergence isolation codebase with errors
+
+# # SampleTargets with randomness removed. Reduced final accuracy
+# aws s3 cp --recursive s3://aws-tensorflow-benchmarking/maskrcnn/results/convergence_codebase_iso_sampletargets_20190320 ~/old_logs/convergence_codebase_iso_sampletargets_20190320
+
+# # Layer 2, Block 5. Converges on bbox but not segm. Unknown cause. Unlikely due to mask loss
+#aws s3 cp --recursive s3://aws-tensorflow-benchmarking/maskrcnn/results/convergence_codebase_iso_roialignmask_and_cropandresizemask_and_maskloss_20190322 ~/old_logs/convergence_codebase_iso_roialignmask_and_cropandresizemask_and_maskloss_20190322
+
+# # Bratin data 20190321. Converges on bbox but not segm. Many flags enabled, problem isolated down to Layer 2, Block 5
+#aws s3 cp --recursive s3://aws-tensorflow-benchmarking/maskrcnn/results/convergence_codebase_iso_bratin_throughput_20190321 ~/old_logs/convergence_codebase_iso_bratin_throughput_20190321
+#aws s3 cp --recursive s3://aws-tensorflow-benchmarking/maskrcnn/results/convergence_codebase_iso_bratin_convergence_20190321 ~/old_logs/convergence_codebase_iso_bratin_convergence_20190321
+
+
+
+
+
+
 
 
 
 
 # sudo rm ~/logs/htimeline.json
 # aws s3 cp --recursive logs/ s3://aws-tensorflow-benchmarking/maskrcnn/results/convergence_codebase_iso_XXXXXXX/
-#aws s3 cp --recursive logs/ s3://aws-tensorflow-benchmarking/maskrcnn/results/convergence_codebase_iso_crop_and_resize_mask_20190321/
 
 
