Fix RPN loss normalization and topk parameter (#26)

MaskRCNN/model_fpn.py
@@ -263,7 +263,7 @@ def multilevel_rpn_losses_batch_fixed_single_image(
 
 @under_name_scope()
 def generate_fpn_proposals_batch_tf_op(multilevel_anchor_boxes,
-        multilevel_box_logits, multilevel_label_logits, orig_image_dims):
+        multilevel_box_logits, multilevel_label_logits, orig_image_dims, batch_size):
     """
     Args:
         multilevel_box_logits:      #lvl [ BS x (NAx4) x H x W ] boxes
@@ -285,7 +285,7 @@ def generate_fpn_proposals_batch_tf_op(multilevel_anchor_boxes,
     all_boxes = []
     all_scores = []
     if cfg.FPN.PROPOSAL_MODE == 'Level':
-        fpn_nms_topk = cfg.RPN.TRAIN_PER_LEVEL_NMS_TOPK if training else cfg.RPN.TEST_PER_LEVEL_NMS_TOPK
+        fpn_nms_topk = cfg.RPN.TRAIN_PER_LEVEL_NMS_TOPK*batch_size if training else cfg.RPN.TEST_PER_LEVEL_NMS_TOPK
         for lvl in range(num_lvl):
             with tf.name_scope(f'Lvl{lvl}'):
                 im_info = tf.cast(orig_images_hw, tf.float32)

MaskRCNN/train.py
@@ -176,7 +176,8 @@ class ResNetFPNModel(DetectionModel):
         proposal_boxes, proposal_scores = generate_fpn_proposals_batch_tf_op(all_anchors_fpn,
                                                                              multilevel_box_logits,
                                                                              multilevel_label_logits,
-                                                                             image_shape2d)
+                                                                             image_shape2d,
+                                                                             cfg.TRAIN.BATCH_SIZE_PER_GPU)
 
 
  
@@ -227,8 +228,8 @@ class ResNetFPNModel(DetectionModel):
                 rpn_losses.extend(si_losses)
 
             with tf.name_scope('rpn_losses'):
-                total_label_loss = tf.add_n(rpn_losses[::2], name='label_loss')
-                total_box_loss = tf.add_n(rpn_losses[1::2], name='box_loss')
+                total_label_loss = tf.truediv(tf.add_n(rpn_losses[::2]), tf.cast(cfg.TRAIN.BATCH_SIZE_PER_GPU, dtype=tf.float32), name='label_loss')
+                total_box_loss = tf.truediv(tf.add_n(rpn_losses[1::2]), tf.cast(cfg.TRAIN.BATCH_SIZE_PER_GPU, dtype=tf.float32), name='box_loss')
                 add_moving_summary(total_label_loss, total_box_loss)
                 losses = [total_label_loss, total_box_loss]
 