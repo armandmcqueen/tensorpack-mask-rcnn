734daabbe657bcc3b4ebcda54e75b60f3b111320
Armand McQueen <armandmcqueen@gmail.com>
2019-04-26 14:12:09 -0700

    Update docker image name

--- docker/build.sh
+++ docker/build.sh
@@ -3,9 +3,9 @@
 BRANCH_NAME=${1:-"master"}
 
 # The BRANCH_NAME refers to the git pull that happens inside of the Dockerfile
-echo "Building docker image tensorpack-mask-rcnn:${BRANCH_NAME}"
+echo "Building docker image tensorpack-mask-rcnn:bai"
 echo ""
 
 
 
-docker build -t tensorpack-mask-rcnn:${BRANCH_NAME} .. --build-arg CACHEBUST=$(date +%s) --build-arg BRANCH_NAME=${BRANCH_NAME}
+docker build -t tensorpack-mask-rcnn:bai .. --build-arg CACHEBUST=$(date +%s) --build-arg BRANCH_NAME=bai
--- docker/run.sh
+++ docker/run.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
-BRANCH_NAME=${1:-"master"}
 
-echo "Running docker image tensorpack-mask-rcnn:dev-${BRANCH_NAME}"
+echo "Running docker image tensorpack-mask-rcnn:bai"
 echo ""
 
 
 
-nvidia-docker run -it  -v ~/data:/data -v ~/logs:/logs tensorpack-mask-rcnn:dev-${BRANCH_NAME}
+nvidia-docker run -it  -v ~/data:/data -v ~/logs:/logs tensorpack-mask-rcnn:bai
