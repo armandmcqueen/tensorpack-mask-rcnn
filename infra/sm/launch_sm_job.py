f6a411dd840881abb04fc1490f403f080b969fcc
Fei Wu <fewu@amazon.com>
2019-08-05 11:39:15 -0700

    add fsx

--- infra/sm/launch_sm_job.py
+++ infra/sm/launch_sm_job.py
@@ -1,6 +1,7 @@
 from sagemaker import get_execution_role
 import sagemaker as sage
 from sagemaker.estimator import Estimator
+from sagemaker.inputs import FileSystemInput
 import datetime
 import subprocess
 import sys
@@ -26,6 +27,11 @@ output_path = 's3://mrcnn-sagemaker/sagemaker_training_release'
 
 s3_path = "s3://armand-ajay-workshop/mask-rcnn/sagemaker/input/train"
 
+lustre_input = FileSystemInput(file_system_id='fs-03f556d03c3c590a2',
+                               file_system_type='FSxLustre',
+                               directory_path='/',
+                               file_system_access_mode='ro')
+
 hyperparams = {"sagemaker_use_mpi": "True",
                "sagemaker_process_slots_per_host": num_gpus,
                "num_gpus":num_gpus,
@@ -36,10 +42,11 @@ estimator = Estimator(image_name, role=sagemaker_iam_role, output_path=output_pa
                       train_instance_count=num_nodes,
                       train_instance_type=instance_type,
                       sagemaker_session=sess,
+                      security_group_ids=['sg-a21b02eb'],
                       train_volume_size=200,
                       base_job_name=job_name,
                       subnets=['subnet-21ac2f2e'],
                       hyperparameters=hyperparams)
 
 #estimator.fit(wait=False)
-estimator.fit({'train':s3_path}, wait=False)
+estimator.fit({'train':lustre_input}, wait=False)
